36e959f87619a104979a0ab71289e44160c8bcb2
tomual <tomual@users.noreply.github.com>
2019-06-10 22:41:57 +0800

    Player walk

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float turnSpeed = 20f;
+    Animator animator;
+    Rigidbody m_rigidbody;
+    Vector3 movement;
+    Quaternion rotation = Quaternion.identity;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        m_rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        movement.Set(horizontal, 0f, vertical);
+        movement.Normalize();
+
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+        animator.SetBool("IsWalking", isWalking);
+
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, movement, turnSpeed * Time.deltaTime, 0f);
+        rotation = Quaternion.LookRotation(desiredForward);
+    }
+
+    private void OnAnimatorMove()
+    {
+        m_rigidbody.MovePosition(m_rigidbody.position + movement * animator.deltaPosition.magnitude);
+        m_rigidbody.MoveRotation(rotation);
+    }
+}
