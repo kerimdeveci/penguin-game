cd2ff0c1d0c79bf6df8122c69ff5d4ad4f44c18b
tomual <tomual@users.noreply.github.com>
2019-06-11 20:02:25 +0800

    Enemy move to waypoints on navmesh

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    public NavMeshAgent navMeshAgent;
+    public Transform[] waypoints;
+    int waypointIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent.SetDestination(waypoints[0].position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance)
+        {
+            waypointIndex = (waypointIndex + 1) % waypoints.Length;
+            navMeshAgent.SetDestination(waypoints[waypointIndex].position);
+        }
+    }
+}
