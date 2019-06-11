693779e928f256dc549c06ad28571b0478efbacb
tomual <tomual@users.noreply.github.com>
2019-06-11 19:42:47 +0800

    NPC interaction - dialogue paging, talk prompt

---
+++ Assets/Scripts/InteractPrompt.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractPrompt : MonoBehaviour
+{
+    public float fadeDuration = 1f;
+    float timer;
+    CanvasGroup canvasGroup;
+    bool fadeIn = false;
+    bool fadeOut = false;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fadeIn)
+        {
+            timer += Time.deltaTime;
+            canvasGroup.alpha = timer / fadeDuration;
+        }
+        if (fadeOut)
+        {
+            timer += Time.deltaTime;
+            canvasGroup.alpha = timer * fadeDuration;
+        }
+    }
+
+    public void OnEnable()
+    {
+        canvasGroup.alpha = 0;
+        fadeIn = true;
+        Debug.Log("OnEnable");
+    }
+
+    public void OnDisable()
+    {
+        if (canvasGroup.alpha == 1)
+        {
+            fadeOut = true;
+            StartCoroutine("Fade");
+        }
+        Debug.Log("OnDisable");
+    }
+
+    IEnumerator Fade()
+    {
+        for (float f = 1f; f >= 0; f -= 0.1f)
+        {
+            yield return new WaitForSeconds(2f);
+        }
+    }
+}
---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,159 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject interactPrompt;
+    public CanvasGroup interactPromptCanvasGroup;
+    bool playerInRange = false;
+    bool talking = false;
+    bool waitingForInput = false;
+
+    List<Page> dialogue;
+    int dialogueCursor = 0;
+    float lastAction;
+    float lastActionCooldown = 0.5f;
+
+    void Awake()
+    {
+        interactPromptCanvasGroup = interactPrompt.GetComponent<CanvasGroup>();
+        interactPromptCanvasGroup.alpha = 0;lastAction = Time.time;
+        lastAction = Time.time;
+
+        dialogue = new List<Page>();
+        dialogue.Add(new Page("Hello", null));
+        dialogue.Add(new Page("The quick brown fox jumps over the lazy dog", null));
+        dialogue.Add(new Page("Pack my box with five dozen liquor jugs", null));
+        dialogue.Add(new Page("The five boxing wizards jump quickly", null));
+    }
+    
+    void Update()
+    {
+        UpdatePrompt();
+        CheckPromptInput();
+        UpdateDialogue();
+        CheckDialogueInput();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            Debug.Log("Player entered interactive area");
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        playerInRange = false;
+        if (other.gameObject == player)
+        {
+            Debug.Log("Player left interactive area");
+        }
+    }
+
+    void UpdateDialogue()
+    {
+        if (talking && !waitingForInput)
+        {
+            NextPage();
+        }
+        
+        if (talking && !playerInRange)
+        {
+            Debug.Log("End Conversation");
+            dialogueCursor = 0;
+            talking = false;
+        }
+    }
+
+    void CheckDialogueInput()
+    {
+        //Debug.Log(talking && waitingForInput && Time.time - lastAction > lastActionCooldown);
+        if (talking && waitingForInput && Time.time - lastAction > lastActionCooldown)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                lastAction = Time.time;
+                waitingForInput = false;
+            }
+        }
+    }
+
+    void NextPage()
+    {
+        if (dialogueCursor >= dialogue.Count)
+        {
+            Debug.Log("End Conversation");
+            dialogueCursor = 0;
+            talking = false;
+            return;
+        }
+        Debug.Log("Turn to page " + dialogueCursor);
+        Debug.Log(dialogue[dialogueCursor].GetMessage());
+        dialogueCursor++;
+        waitingForInput = true;
+    }
+
+    void CheckPromptInput()
+    {
+        if (playerInRange && !talking)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                lastAction = Time.time;
+                Debug.Log("Start conversation");
+                talking = true;
+            }
+        }
+    }
+
+    void UpdatePrompt()
+    {
+        if (playerInRange && !talking)
+        {
+            interactPromptCanvasGroup.alpha += 0.2f;
+        }
+        else
+        {
+            interactPromptCanvasGroup.alpha -= 0.2f;
+        }
+    }
+}
+
+public class Page
+{
+    private string message;
+    private OrderedDictionary options;
+
+    public Page(string message)
+    {
+        this.message = message;
+        this.options = null;
+    }
+
+    public Page(string message, OrderedDictionary options)
+    {
+        this.message = message;
+        this.options = options;
+    }
+
+    public bool HasOptions()
+    {
+        return this.options != null;
+    }
+
+    public string GetMessage()
+    {
+        return message;
+    }
+
+    public OrderedDictionary GetOptions()
+    {
+        return options;
+    }
+}
